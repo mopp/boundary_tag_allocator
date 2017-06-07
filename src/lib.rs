931945c3d02986933410b3490ed87fe2600499d6
mopp <hello@mopp.jp>
2017-06-08 08:44:14 +0900

    fix: change return value of next_tag_of to pass the borrow checker

--- src/lib.rs
+++ src/lib.rs
@@ -78,15 +78,15 @@ impl<'a> BoundaryTag {
         (tag, Some(new_tag))
     }
 
-    fn next_tag_of(tag: &'a mut BoundaryTag) -> Option<&'a mut BoundaryTag>
+    fn next_tag_of(tag: &'a mut BoundaryTag) -> (&'a mut BoundaryTag, Option<&'a mut BoundaryTag>)
     {
         if tag.is_sentinel {
-            return None;
+            return (tag, None);
         }
 
         let addr = tag.addr() + tag.size;
         let next_tag = unsafe { &mut *(addr as *mut BoundaryTag) };
-        Some(next_tag)
+        (tag, Some(next_tag))
     }
 }
 
